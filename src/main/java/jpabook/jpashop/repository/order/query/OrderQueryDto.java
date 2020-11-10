dec8d01b89c001a7f2b044fb2ec6df04b5b35ed8
kbj <qudwns6122@naver.com>
2020-11-10 19:23:22 +0900

    "11-10"

--- src/main/java/jpabook/jpashop/repository/order/query/OrderQueryDto.java
+++ src/main/java/jpabook/jpashop/repository/order/query/OrderQueryDto.java
@@ -3,13 +3,17 @@ package jpabook.jpashop.repository.order.query;
 import jpabook.jpashop.domain.Address;
 import jpabook.jpashop.domain.OrderStatus;
 import lombok.Data;
+import lombok.EqualsAndHashCode;
 
 import java.time.LocalDateTime;
 import java.util.List;
 
 @Data
+//orderId를 기준으로 collect(groupingBy 할 때 묶어준다.
+@EqualsAndHashCode(of = "orderId")
 public class OrderQueryDto {
 
+    //기준.
     private Long orderId;
     private String name;
     private LocalDateTime orderDate;
@@ -23,5 +27,15 @@ public class OrderQueryDto {
         this.orderDate = orderDate;
         this.orderStatus = orderStatus;
         this.address = address;
+    }
+
+    // 컬렉션까지 한번에 넣는거.
+    public OrderQueryDto(Long orderId, String name, LocalDateTime orderDate, OrderStatus orderStatus, Address address, List<OrderItemQueryDto> orderItems) {
+        this.orderId = orderId;
+        this.name = name;
+        this.orderDate = orderDate;
+        this.orderStatus = orderStatus;
+        this.address = address;
+        this.orderItems = orderItems;
     }
 }
